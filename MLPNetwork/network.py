40b072936e13e53cf199da8617af0da0fa27bfb1
Marian Gafitescu <pmgafitescu@gmail.com>
2019-01-09 12:49:24 +0200

    Added datasets for training and testing

--- MLPNetwork/network.py
+++ MLPNetwork/network.py
@@ -12,53 +12,54 @@ def flatten(x):
         return [x]
 
 
-f = open("data.json", "r")
-a = json.load(f)
+input_shape = 706
+file = "training_features.json"
 features = []
-for i in range(0, 471):
-    nested_lists = a.get(str(i))
+f = open(file, "r")
+a = json.load(f)
+for nested_lists in a.values():
     flat_list = flatten(nested_lists)
     features.append(flat_list)
 f.close()
 
-f = open("labels.txt", "r", encoding="utf-8")
+
+f = open("training_labels.txt", "r", encoding="utf-8")
 labels = []
 for line in f:
     line = line.replace("\n", "")
     labels.append(line)
 
-labels_set = set(labels)
+f = open("labelsset.txt", "r", encoding="utf-8")
+labels_set = []
+for line in f:
+    line = line.replace("\n", "")
+    labels_set.append(line)
+f.close()
+classes = len(labels_set)
+print(classes)
 labels_mapping = dict()
 labels_mapping_reverse = dict()
 for index, label in enumerate(labels_set):
     labels_mapping[label] = index
     labels_mapping_reverse[index] = label
 
-l1 = open("labelsset.txt", "w", encoding="utf-8")
-for e in labels_set:
-    l1.write(e)
-    l1.write("\n")
-
-features = features[:446]
+features = features
 labels = [labels_mapping[label] for label in labels]
 
 features = np.array(features)
 features = features / 128
-x_train = features[50:]
-y_train = keras.utils.to_categorical(labels[50:], num_classes=33)
-x_test = features[:50]
-y_test = keras.utils.to_categorical(labels[:50], num_classes=33)
+x_train = features
+y_train = keras.utils.to_categorical(labels, num_classes=classes)
 
 model = keras.models.Sequential()
 keras.initializers.VarianceScaling(scale=1.0, mode='fan_in', distribution='normal', seed=None)
-model.add(InputLayer(input_shape=(5824,)))
-model.add(Dense(1000, input_shape=(5824,), activation="relu"))
-model.add(Dense(100, input_shape=(5824,), activation="relu"))
-model.add(Dense(33, input_shape=(5824,), activation="softmax"))
+model.add(InputLayer(input_shape=(input_shape,)))
+model.add(Dense(500, input_shape=(input_shape,), activation="relu"))
+model.add(Dense(classes, input_shape=(input_shape,), activation="softmax"))
 
-model.compile(optimizer=keras.optimizers.Adadelta(),
+model.compile(optimizer=keras.optimizers.Adam(),
               loss="categorical_crossentropy",
               metrics=["accuracy"])
-model.fit(x_train, y_train, epochs=20, batch_size=5)
+model.fit(x_train, y_train, epochs=10, batch_size=5)
 
 keras.models.save_model(model, "saved_model.h5", overwrite=True, include_optimizer=True)
